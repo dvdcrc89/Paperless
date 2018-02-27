00c08a1253acd6ecf55919505ce73aa5b329a503
dvdcrc89 <dvdcrc89@gmail.com>
2018-02-27 11:33:02 +0000

    fixed autorender on stocktakes

--- client/main.js
+++ client/main.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import {Meteor} from 'meteor/meteor'
 import {Tracker} from 'meteor/tracker'
 import {Items} from "./../imports/api/items";
+import {Temps} from "./../imports/api/temps";
+
 import StockMain from '../imports/ui/stock/stockMain'
 import StockTakes from './../imports/ui/stocktakes/stocktakes'
 import FridgeFreezer from './../imports/ui/paperworks/fridgeFreezer'
@@ -16,6 +18,7 @@ Meteor.startup(()=> {
     Tracker.autorun(()=> {
     onAuthChange();
         const data = Items.find({User: Meteor.userId()}).fetch()
+        const temps = Temps.find({User: Meteor.userId()}).fetch()
         ReactDOM.render(<AppRouter/>,document.getElementById("app"));
 })
 });
