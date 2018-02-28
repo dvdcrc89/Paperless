f5b2215429c4df9971a15a186b5ae55210ba3d42
dvdcrc89 <dvdcrc89@gmail.com>
2018-02-28 18:50:59 +0000

    Font awesome and nav

--- imports/ui/generic/titlebar.js
+++ imports/ui/generic/titlebar.js
@@ -10,6 +10,61 @@ export default class TitleBar extends React.Component{
         var begun = moment(date).format("DD MMMM YYYY");
         return begun;
     }
+    renderNav(){
+        if(!!Meteor.userId()){
+            return(
+                <div className="navbar">
+                    <div className="navbarcontainer">
+
+                        <div className="dropdown">
+                            <h3 className="dropbtn">
+                                STOCK
+                                <i className="fa fa-caret-down"></i>
+                            </h3>
+                            <div className="dropdown-content">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
+                        </div>
+                        <div className="dropdown">
+                            <h3 className="dropbtn">
+                                MENU
+                                <i className="fa fa-caret-down"></i>
+                            </h3>
+                            <div className="dropdown-content">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
+                        </div>
+                        <div className="dropdown">
+                            <h3 className="dropbtn">
+                                PAPERWORK
+                                <i className="fa fa-caret-down"></i>
+                            </h3>
+                            <div className="dropdown-content">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
+                        </div>
+                        <div className="dropdown">
+                            <h3 className="dropbtn">
+                                EXPENSES
+                                <i className="fa fa-caret-down"></i>
+                            </h3>
+                            <div className="dropdown-content">
+                                <a href="#">Link 1</a>
+                                <a href="#">Link 2</a>
+                                <a href="#">Link 3</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+    }
 
     render(){
         let date=this.returnDate();
@@ -19,9 +74,12 @@ export default class TitleBar extends React.Component{
                 <div className="titleBar">
                     <h2 className="titleDate">{date}</h2>
                     <h1>{this.props.title}</h1>
+
                     <h2 className="titleMainData" >{this.props.mainData}</h2>
                 </div>
+                {this.renderNav()}
             </div>
+
         )
     }
 }
